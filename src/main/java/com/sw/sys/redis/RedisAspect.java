03b4a753acf1654915e1bb8074c11bbffed59d98
白扣衬衫 <1293332462@qq.com>
2020-02-14 16:35:49 +0800

    添加AOP切面处理缓存

--- src/main/java/com/sw/sys/redis/RedisAspect.java
+++ src/main/java/com/sw/sys/redis/RedisAspect.java
@@ -22,26 +22,24 @@ public class RedisAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(RedisAspect.class);
 
     @Autowired
-    @Qualifier("RedisUtil")
     private RedisUtil redisUtil;
 
     /**
      * 拦截所有元注解RedisCache注解的方法
      */
     @Pointcut(value = "@annotation(com.sw.sys.redis.RedisCache)")
-    public void pointcutMethod() {
+    public void pointcutMethod(){
 
     }
 
     /**
      * 环绕处理，先从Redis里获取缓存,查询不到，就查询MySQL数据库，
      * 然后再保存到Redis缓存里
-     *
      * @param joinPoint
      * @return
      */
     @Around("pointcutMethod()")
-    public Object around(ProceedingJoinPoint joinPoint) {
+    public Object around(ProceedingJoinPoint joinPoint){
         //前置：从Redis里获取缓存
         //先获取目标方法参数
         long startTime = System.currentTimeMillis();
@@ -63,27 +61,27 @@ public class RedisAspect {
 
         Object obj = redisUtil.get(redisKey);
 
-        if (obj != null) {
+        if(obj!=null){
             LOGGER.info("**********从Redis中查到了数据**********");
-            LOGGER.info("Redis的KEY值:" + redisKey);
-            LOGGER.info("REDIS的VALUE值:" + obj.toString());
+            LOGGER.info("Redis的KEY值:"+redisKey);
+            LOGGER.info("REDIS的VALUE值:"+obj.toString());
             return obj;
         }
         long endTime = System.currentTimeMillis();
-        LOGGER.info("Redis缓存AOP处理所用时间:" + (endTime - startTime));
+        LOGGER.info("Redis缓存AOP处理所用时间:"+(endTime-startTime));
         LOGGER.info("**********没有从Redis查到数据**********");
-        try {
+        try{
             obj = joinPoint.proceed();
-        } catch (Throwable e) {
+        }catch(Throwable e){
             e.printStackTrace();
         }
         LOGGER.info("**********开始从MySQL查询数据**********");
         //后置：将数据库查到的数据保存到Redis
-        Boolean code = redisUtil.set(redisKey, obj);
-        if (code) {
+        Boolean code = redisUtil.set(redisKey,obj);
+        if(code){
             LOGGER.info("**********数据成功保存到Redis缓存!!!**********");
-            LOGGER.info("Redis的KEY值:" + redisKey);
-            LOGGER.info("REDIS的VALUE值:" + obj.toString());
+            LOGGER.info("Redis的KEY值:"+redisKey);
+            LOGGER.info("REDIS的VALUE值:"+obj.toString());
         }
         return obj;
     }
